1a0c321c2c3ad60238ffe2f59d1ea727aac7a221
John Kelvie <jpk@bespoken.io>
2019-12-31 11:27:39 -0500

    Added more debug output

--- src/batch-runner.js
+++ src/batch-runner.js
@@ -78,6 +78,7 @@ class BatchRunner {
   }
 
   async _processRecord (device, record) {
+    console.log(`PROCESS run: ${this._job.run} utterance: ${record.utterance}`)
     // Do just-in-time processing on the record
     await Source.instance().loadRecord(record)
 
