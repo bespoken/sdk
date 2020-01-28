5f9d9903211bbe74aa8148945c763b6db86f194c
John Kelvie <jpk@bespoken.io>
2020-01-27 19:30:00 -0500

    Added device tags

--- src/device.js
+++ src/device.js
@@ -7,6 +7,7 @@ class Device {
   constructor (token, skipSTT = false) {
     this._token = token
     this._skipSTT = skipSTT
+    this._tags = []
   }
 
   async message (voiceId, messages, attempt = 1) {
@@ -17,7 +18,7 @@ class Device {
       voiceID: voiceId
     })
 
-    virtualDevice.baseURL = Config.get('virtualDeviceBaseURL')
+    virtualDevice.baseURL = Config.get('virtualDeviceBaseURL', [], false, 'https://virtual-device.bespoken.io')
 
     try {
       const messagesArray = []
@@ -53,11 +54,30 @@ class Device {
         throw error
       }
 
+      console.log('UTTERANCE: ' + JSON.stringify(messages, null, 2))
       const backoffTime = 10000
       console.error(`DEVICE MESSAGE error: ${error} retrying ${backoffTime / 1000} seconds`)
       await util.sleep(backoffTime)
       return this.message(voiceId, messages, attempt + 1)
     }
+  }
+
+  addTag (tag) {
+    this._tags.push(tag)
+  }
+
+  hasTags (tags) {
+    let success = true
+    tags.forEach((tag) => {
+      if (this._tags.indexOf(tag) === -1) {
+        success = false
+      }
+    })
+    return success
+  }
+
+  get tags () {
+    return this._tags
   }
 
   get token () {
@@ -85,28 +105,33 @@ class DevicePool {
     }
     // Create a device for each token
     tokens.forEach(token => {
-      console.log(`DEVICE create token: ${token} skipSTT: ${skipSTT}`)
-      this._devices.push(new Device(token.trim(), skipSTT))
+      const tags = tokensInfo[token].map(tag => tag.trim())
+      console.log(`DEVICE create token: ${token} skipSTT: ${skipSTT} tags: ${tags}`)
+      const device = new Device(token.trim(), skipSTT)
+
+      // Add the tags to the device
+      tags.forEach(tag => device.addTag(tag))
+      this._devices.push(device)
     })
 
     // Create an array to track the devices currently in use
     this._devicesInUse = []
   }
 
-  async lock () {
-    // Check if a free token is available
-    console.log('DEVICE LOCK attempt - devices available: ' + this._freeCount())
-    while (this._freeCount() === 0) {
+  async lock (record) {
+    console.log(`DEVICE LOCK tags: ${record.deviceTags} count: ${this._freeDevices(record).length}`)
+    while (this._freeDevices(record).length === 0) {
       await util.sleep(1000)
     }
 
     // If there is a free token, add to our list of tokens in use so no one else can use it
-    const device = this._devices.find((device) => {
+    const device = this._freeDevices(record).find((device) => {
       return this._devicesInUse.find(d => d.token === device.token) === undefined
     })
-    console.log('DEVICE LOCK acquire - devices available: ' + this._freeCount() + ' TOKEN: ' + device.token)
 
     this._devicesInUse.push(device)
+    console.log(`DEVICE LOCK acquire tags: ${record.deviceTags} available: ${this._freeDevices(record).length} token: ${device.token}`)
+
     return device
   }
 
@@ -114,6 +139,19 @@ class DevicePool {
     // Remove the token we used from our tokens in list use - i.e., return it to the free pool
     this._devicesInUse = _.pull(this._devicesInUse, device)
     console.log('DEVICE FREE ' + device._token + ' tokens available: ' + this._freeCount())
+  }
+
+  _freeDevices (record) {
+    // Filter to the devices that have tags that match the record
+    const validDevices = this._devices.filter(device => {
+      return device.hasTags(record.deviceTags)
+    })
+
+    // Then filter down to those that are available
+    const freeDevices = validDevices.filter(device => {
+      return !this._devicesInUse.find(d => d.token === device.token)
+    })
+    return freeDevices
   }
 
   _freeCount () {
