c814f64f277ed114d4356b665349327c3e22caf1
Moisés Ñañez <moisesnandres@gmail.com>
2020-05-21 11:24:29 -0500

    Save job when there is an error on the process if the job key exists

--- bin/bbt.js
+++ bin/bbt.js
@@ -18,6 +18,10 @@ program
     const runner = new BatchRunner(config, options.output_file)
     runner.process().then(() => {
       console.log('RUNNER DONE!')
+    }).catch(() => {
+      if (runner._job.key) {
+        runner._synchronizer.saveJob('ERROR')
+      }
     })
   })
 
