0688c84018949be20f196582528813a4cc881700
John Kelvie <jpk@bespoken.io>
2022-07-18 15:51:42 -0400

    Mirrored latest changes

--- packages/collect-sdk/lib/collector.js
+++ packages/collect-sdk/lib/collector.js
@@ -8,15 +8,11 @@ const Reply = require('@bespoken-api/nlp-sdk/lib/reply')
  *
  */
 class Collector {
-  constructor() {
-
-  }
-
   /**
    * @param {Message} message
    * @returns {Promise<Message>}
    */
-  async collectMessage(message) {
+  async collectMessage (message) {
     const baseURL = Env.requiredVariable('COLLECT_API_URL', 'https://collect.bespoken.io/collect')
     const url = `${baseURL}/message`
     logger.debug('collect message: ' + message.toStringAsJSON())
@@ -25,25 +21,25 @@ class Collector {
         'content-type': 'application/json'
       }
     })
-    
+
     const savedMessage = response.data
     message.id = savedMessage.id
     message.conversation.id = savedMessage.conversation.id
     return message
-  } 
-  
+  }
+
   /**
    * @param {Reply} reply
    * @returns {Promise<void>}
    */
-  async collectReply(reply) {
+  async collectReply (reply) {
     const baseURL = Env.requiredVariable('COLLECT_API_URL', 'https://collect.bespoken.io/collect')
     const url = `${baseURL}/reply`
     logger.debug('collect reply: ' + JSON.stringify(reply))
     const response = await axios.post(url, reply)
-    
+
     return response.data
-  } 
+  }
 }
 
 module.exports = Collector
